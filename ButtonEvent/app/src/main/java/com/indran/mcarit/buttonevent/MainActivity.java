8c9e9e1706cd04c1f7cb3e6a82ee7505e68098d5
indrajith <indrajithc@gmail.com>
2018-02-20 09:48:23 +0530

    lab cycle 1 , 2nd program

---
+++ ButtonEvent/app/src/main/java/com/indran/mcarit/buttonevent/MainActivity.java
@@ -0,0 +1,19 @@
+package com.indran.mcarit.buttonevent;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void toastMe(View view) {
+        Toast.makeText(this, "Hello Indra !! ", Toast.LENGTH_SHORT).show();
+    }
+}
