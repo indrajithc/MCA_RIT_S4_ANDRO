9e51964bfd83450c343a97ee2f91b21ab1ab6a8a
indrajith <indrajithc@gmail.com>
2018-02-20 09:40:38 +0530

    lab cycle 1 , 1st program

---
+++ Toast/app/src/main/java/com/indran/mcarit/toast/MainActivity.java
@@ -0,0 +1,20 @@
+package com.indran.mcarit.toast;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void toastMe(View view) {
+
+        Toast.makeText(this, "Hello RIT !! ", Toast.LENGTH_SHORT).show();
+    }
+}
