40bb03137faa1e8f7dc47aa02fb70eb83da18748
indran <indranspeaks@gmail.com>
2018-03-23 11:58:38 +0530

    5th cycle 3rd

---
+++ OnlineRegistration/app/src/main/java/com/indran/onlineregistration/DatabaseHelper.java
@@ -0,0 +1,74 @@
+package com.indran.onlineregistration;
+
+/**
+ * Created by stud on 23/3/18.
+ */
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+/**
+ * Created by ProgrammingKnowledge on 4/3/2015.
+ */
+public class DatabaseHelper extends SQLiteOpenHelper {
+    public static final String DATABASE_NAME = "indran.db";
+    public static final String TABLE_NAME = "basic_details";
+    public static final String COL_1 = "ID";
+    public static final String COL_2 = "NAME";
+    public static final String COL_3 = "DOB";
+    public static final String COL_4 = "ADDRESS";
+
+    public DatabaseHelper(Context context) {
+        super(context, DATABASE_NAME, null, 1);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL("create table " + TABLE_NAME +
+                " (ID INTEGER PRIMARY KEY AUTOINCREMENT,NAME TEXT,DOB TEXT,ADDRESS TEXT)");
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL("DROP TABLE IF EXISTS "+TABLE_NAME);
+        onCreate(db);
+    }
+
+    public boolean insertData(String name,String dob,String address) {
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues contentValues = new ContentValues();
+        contentValues.put(COL_2,name);
+        contentValues.put(COL_3,dob);
+        contentValues.put(COL_4,address);
+        long result = db.insert(TABLE_NAME,null ,contentValues);
+        if(result == -1)
+            return false;
+        else
+            return true;
+    }
+
+    public Cursor getAllData() {
+        SQLiteDatabase db = this.getWritableDatabase();
+        Cursor res = db.rawQuery("select * from "+TABLE_NAME,null);
+        return res;
+    }
+
+    public boolean updateData(String id,String name,String dob,String address) {
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues contentValues = new ContentValues();
+        contentValues.put(COL_1,id);
+        contentValues.put(COL_2,name);
+        contentValues.put(COL_3,dob);
+        contentValues.put(COL_4,address);
+        db.update(TABLE_NAME, contentValues, "ID = ?",new String[] { id });
+        return true;
+    }
+
+    public Integer deleteData (String id) {
+        SQLiteDatabase db = this.getWritableDatabase();
+        return db.delete(TABLE_NAME, "ID = ?",new String[] {id});
+    }
+}
---
+++ OnlineRegistration/app/src/main/java/com/indran/onlineregistration/Registration.java
@@ -0,0 +1,17 @@
+package com.indran.onlineregistration;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class Registration extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registration);
+    }
+
+    public void registration(View view) {
+    }
+}
