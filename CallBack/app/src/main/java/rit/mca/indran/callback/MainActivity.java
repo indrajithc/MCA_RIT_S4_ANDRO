88d325e09f170ecb80829d8884aaa905707fe6b1
indran <indranspeaks@gmail.com>
2018-04-05 04:56:19 +0530

    4th cycle 1st q

---
+++ CallBack/app/src/main/java/rit/mca/indran/callback/MainActivity.java
@@ -0,0 +1,20 @@
+package rit.mca.indran.callback;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void toNext(View view) {
+        Intent i = new Intent(getApplicationContext(),NextActivity.class);
+        startActivity(i);
+    }
+}
---
+++ CallBack/app/src/main/java/rit/mca/indran/callback/NextActivity.java
@@ -0,0 +1,31 @@
+package rit.mca.indran.callback;
+
+import android.support.v4.app.NavUtils;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.MenuItem;
+
+public class NextActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_next);
+
+
+//        getActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                NavUtils.navigateUpFromSameTask(this);
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+}
