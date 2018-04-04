5e52728038eacb992430ecd8fe7ea8058d4cb8af
indran <indranspeaks@gmail.com>
2018-04-04 06:08:08 +0530

    temp pdfs added

--- AlarmClock/app/src/main/java/com/indran/alarmclock/AlarmReceiver.java
+++ AlarmClock/app/src/main/java/com/indran/alarmclock/AlarmReceiver.java
@@ -4,14 +4,14 @@ package com.indran.alarmclock;
  * Created by indran on 3/22/18.
  */
 
-        import android.app.Activity;
-        import android.content.ComponentName;
-        import android.content.Context;
-        import android.content.Intent;
-        import android.media.Ringtone;
-        import android.media.RingtoneManager;
-        import android.net.Uri;
-        import android.support.v4.content.WakefulBroadcastReceiver;
+import android.app.Activity;
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+import android.media.Ringtone;
+import android.media.RingtoneManager;
+import android.net.Uri;
+import android.support.v4.content.WakefulBroadcastReceiver;
 
 public class AlarmReceiver extends WakefulBroadcastReceiver {
 
@@ -34,7 +34,7 @@ public class AlarmReceiver extends WakefulBroadcastReceiver {
 
         //this will send a notification message
         ComponentName comp = new ComponentName(context.getPackageName(),
-                AlarmService.class.getName());
+            AlarmService.class.getName());
         startWakefulService(context, (intent.setComponent(comp)));
         setResultCode(Activity.RESULT_OK);
     }
