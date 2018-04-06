11886a362b47d855426bef5085e8186f4075ec5a
Indrajith C <indrajithc@hotmail.com>
2018-04-06 10:48:18 +0530

    Update DatabaseHelper.java

--- OnlineRegistration/app/src/main/java/com/indran/onlineregistration/DatabaseHelper.java
+++ OnlineRegistration/app/src/main/java/com/indran/onlineregistration/DatabaseHelper.java
@@ -1,18 +1,12 @@
 package com.indran.onlineregistration;
 
-/**
- * Created by stud on 23/3/18.
- */
-
 import android.content.ContentValues;
 import android.content.Context;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
-/**
- * Created by ProgrammingKnowledge on 4/3/2015.
- */
+
 public class DatabaseHelper extends SQLiteOpenHelper {
     public static final String DATABASE_NAME = "indran.db";
     public static final String TABLE_NAME = "basic_details";
